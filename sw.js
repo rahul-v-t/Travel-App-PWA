a32c35803b9c688001080bf76efef124559b85db
rahulthambi10@gmail.com <rahulthambi10@gmail.com>
2021-11-03 10:51:18 +0530

    updated

--- sw.js
+++ sw.js
@@ -2,8 +2,27 @@ self.addEventListener("install", function (event) {
     console.log("Installed",event);
     event.waitUntil(
         caches.open("static").then((cache)=>{
-            cache.add("/js/jquery-3.5.1.min.js");
-            cache.add("/js/script.js");
+            cache.addAll([
+                "/",
+                "/index.html",
+                "/manifest.json",
+
+                "/js/script.js",
+                "/js/jquery-3.5.1.min.js",
+
+                "/css/style.css",
+
+                "/images/logo.svg",
+                "images/search.svg",
+
+                "/fonts/product_sans_bold-webfont.woff2",
+                "/fonts/product_sans_bold-webfont.woff",
+                "/fonts/product_sans_regular-webfont.woff2",
+                "/fonts/product_sans_regular-webfont.woff",
+
+                "https://traveller.talrop.works/api/v1/places/",
+                "https://traveller.talrop.works/api/v1/places/categories/",
+            ])
         })
     );
     
@@ -15,5 +34,13 @@ self.addEventListener("activate",function (event) {
 });
 
 self.addEventListener("fetch", function (event) {
-    event.respondwith(fecth(event.request));
+    event.respondWith(
+        caches.match(event.request).then((response)=>{
+            if (response){
+                return response;
+            } else {
+                return fetch(event.request);
+            }
+        })
+    );
 });
